ce5fe2ac1936dd3efb5efbd06154c199ee05e2c9
TheCrow17 <erickaricari@gmail.com>
2026-05-30 20:33:50 -0400

    config(main): config cors and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
@@ -19,7 +33,7 @@ async function bootstrap() {
 
   await app.startAllMicroservices();
   Logger.log('Microservice is listening', 'Bootstrap');
-  
+
   await app.listen(port, () => {
     Logger.log(`Server is running on port ${port}`, 'Bootstrap');
   });
